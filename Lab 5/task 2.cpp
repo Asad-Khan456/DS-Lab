040d81f7585eb121fd13524255c47bcf7b968a95
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-09-28 23:41:31 +0500

    Update task 2.cpp

--- Lab 5/task 2.cpp
+++ Lab 5/task 2.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
 using namespace std;
-
-void printNumbers(int n) {
-    if (n == 0) {
+void printNumbers(int n){
+    if(n == 0){
         return;
     }
     cout << n << " ";
     printNumbers(n - 1);
 }
-
 void functionB(int n); 
-
-void functionA(int n) {
-    if (n <= 0) {
+void functionA(int n){
+    if(n <= 0){
         return;
     }
     cout << "A: " << n << " ";
     functionB(n - 1);
 }
-
-void functionB(int n) {
-    if (n <= 0) {
+void functionB(int n){
+    if(n <= 0){
         return;
     }
-    cout << "B: " << n << " ";
+    cout << "b; "<< n << " ";
     functionA(n - 1);
 }
-
-int main() {
+int main(){
     int num;
-    cout << "Enter a number: ";
+    cout << "Enter a num: ";
     cin >> num;
-
-    cout << "\nDirect Recursion (numbers from " << num << " down to 1):\n";
+    cout << "\nDirect recursion (num from " << num << " down to 1):" << endl;
     printNumbers(num);
-
-    cout << "\n\nIndirect Recursion (alternating A and B calls):\n";
+    cout << "\n\nIndirect Recursion (alternating a and b calls):" << endl;
     functionA(num);
-
     cout << endl;
     return 0;
 }
