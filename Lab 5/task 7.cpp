8c1700e370d39838c856b28514e18fb0e3285928
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-09-28 23:53:54 +0500

    Update task 7.cpp

--- Lab 5/task 7.cpp
+++ Lab 5/task 7.cpp
@@ -2,68 +2,73 @@
 #include <vector>
 using namespace std;
 
-class NQueens {
+
+class NQueens{
     int N;
-
-    bool isSafe(vector<vector<int>>& board, int row, int col) {
-        for (int i = 0; i < row; i++)
+    bool isSafe(vector<vector<int>>& board, int row, int col){
+        for(int i = 0; i < row; i++){
             if (board[i][col] == 1)
+            {
                 return false;
-
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j] == 1)
+            }
+        }
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if(board[i][j] == 1)
+            {
                 return false;
-
-        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++)
-            if (board[i][j] == 1)
+            }
+        }
+        for(int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++){
+            if(board[i][j] == 1)
+            {
                 return false;
-
+            }
+        }
         return true;
     }
 
-    void solveNQUtil(vector<vector<int>>& board, int row) {
-        if (row == N) {
+
+    void solveNQUtil(vector<vector<int>>& board, int row){
+        if(row == N){
             printSolution(board);
             return;
         }
-
-        for (int col = 0; col < N; col++) {
-            if (isSafe(board, row, col)) {
+        for(int col = 0; col < N; col++){
+            if(isSafe(board, row, col)){
                 board[row][col] = 1;
                 solveNQUtil(board, row + 1);
                 board[row][col] = 0;
             }
         }
     }
-
-    void printSolution(vector<vector<int>>& board) {
+    void printSolution(vector<vector<int>>& board){
         static int solutionCount = 1;
         cout << "Solution " << solutionCount++ << ":\n";
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                cout << (board[i][j] == 1 ? "Q " : ". ");
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(board[i][j] == 1){
+                    cout << "Q ";
+                }
+                else{
+                    cout << ". ";
+                }
             }
             cout << endl;
         }
         cout << endl;
     }
-
 public:
-    NQueens(int n) : N(n) {}
-
-    void solve() {
+    NQueens(int n) : N(n){}
+    void solve(){
         vector<vector<int>> board(N, vector<int>(N, 0));
         solveNQUtil(board, 0);
     }
 };
-
-int main() {
+int main(){
     int n;
-    cout << "Enter the value of N: ";
+    cout << "Enter the value of n: ";
     cin >> n;
-
     NQueens nq(n);
     nq.solve();
-
     return 0;
 }
