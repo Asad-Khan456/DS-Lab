daea8ca714e7dd8bbad4196c4b2403a7f8996737
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-09-28 23:39:19 +0500

    Update task 1.cpp

--- Lab 5/task 1.cpp
+++ Lab 5/task 1.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
-
-class Factorial {
+class Factorial{
 public:
-    int calculateFactorial(int n) {
-        if (n == 0 || n == 1) {
+    int calculateFactorial(int n){
+        if(n == 0 || n == 1){
             return 1;
         }
         return n * calculateFactorial(n - 1);
     }
 };
-
-int main() {
+int main(){
     Factorial f;
     int number;
     cout << "Enter a number: ";
     cin >> number;
-    cout << "Factorial of " << number << " = " 
+    cout << "Fctorial of " << number << " = " 
          << f.calculateFactorial(number) << endl;
     return 0;
 }
