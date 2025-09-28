e38e50f4f98db0a178026a3d7928e3620b19f7d4
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-09-28 23:47:46 +0500

    Update task 5.cpp

--- Lab 5/task 5.cpp
+++ Lab 5/task 5.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
 using namespace std;
-
 const int SIZE = 9;
-
-bool isValid(int grid[SIZE][SIZE], int row, int col, int num) {
-    for (int x = 0; x < SIZE; x++) {
-        if (grid[row][x] == num || grid[x][col] == num) {
+bool isValid(int grid[SIZE][SIZE], int row, int col, int num){
+    for(int x = 0; x < SIZE; x++){
+        if (grid[row][x] == num || grid[x][col] == num)
+        {
             return false;
         }
     }
-
     int startRow = row - row % 3;
     int startCol = col - col % 3;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (grid[i + startRow][j + startCol] == num) {
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(grid[i + startRow][j + startCol] == num)
+            {
                 return false;
             }
         }
     }
-
     return true;
 }
-
-bool solveSudoku(int grid[SIZE][SIZE]) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
-            if (grid[row][col] == 0) {
-                for (int num = 1; num <= 9; num++) {
-                    if (isValid(grid, row, col, num)) {
+bool solveSudoku(int grid[SIZE][SIZE]){
+    for(int row = 0; row < SIZE; row++)
+    {
+        for(int col = 0; col < SIZE; col++)
+        {
+            if(grid[row][col] == 0)
+            {
+                for(int num = 1; num <= 9; num++)
+                {
+                    if(isValid(grid, row, col, num))
+                    {
                         grid[row][col] = num;
-                        if (solveSudoku(grid)) {
+                        if(solveSudoku(grid))
+                        {
                             return true;
                         }
-                        grid[row][col] = 0; // backtrack
+                        grid[row][col] = 0; 
                     }
                 }
                 return false;
@@ -42,18 +47,18 @@ bool solveSudoku(int grid[SIZE][SIZE]) {
     }
     return true;
 }
-
-void printGrid(int grid[SIZE][SIZE]) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
+void printGrid(int grid[SIZE][SIZE]){
+    for(int row = 0; row < SIZE; row++)
+    {
+        for(int col = 0; col < SIZE; col++)
+        {
             cout << grid[row][col] << " ";
         }
         cout << endl;
     }
 }
-
-int main() {
-    int grid[SIZE][SIZE] = {
+int main(){
+    int grid[SIZE][SIZE] ={
         {5,3,0, 0,7,0, 0,0,0},
         {6,0,0, 1,9,5, 0,0,0},
         {0,9,8, 0,0,0, 0,6,0},
@@ -66,13 +71,12 @@ int main() {
         {0,0,0, 4,1,9, 0,0,5},
         {0,0,0, 0,8,0, 0,7,9}
     };
-
-    if (solveSudoku(grid)) {
-        cout << "Solved Sudoku:" << endl;
+    if(solveSudoku(grid)){
+        cout << "Solved Sudoku;" << endl;
         printGrid(grid);
-    } else {
-        cout << "No solution exists!" << endl;
     }
-
+    else{
+        cout << "No solution exist" << endl;
+    }
     return 0;
 }
