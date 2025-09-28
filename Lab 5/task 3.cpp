93b52e9c0a7a4df97d51f4486c3f54dd7c894316
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-09-28 23:42:36 +0500

    Update task 3.cpp

--- Lab 5/task 3.cpp
+++ Lab 5/task 3.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int sumTail(int n, int total) {
-    if (n == 0) {
+
+int sumTail(int n, int total){
+    if(n == 0){
         return total;
     }
     return sumTail(n - 1, total + n);
 }
-
-int sumNonTail(int n) {
-    if (n == 0) {
+int sumNonTail(int n){
+    if(n == 0){
         return 0;
     }
     return n + sumNonTail(n - 1);
 }
-
-int main() {
+int main(){
     int num;
-    cout << "Enter a number: ";
+    cout << "Enter a num:";
     cin >> num;
-
-    cout << "Sum using Tail Recursion: " << sumTail(num, 0) << endl;
-    cout << "Sum using Non-Tail Recursion: " << sumNonTail(num) << endl;
-
+    cout << "Sum using Tail rceursion: " << sumTail(num, 0) << endl;
+    cout << "Sum using Non Tail recursion: " << sumNonTail(num) << endl;
     return 0;
 }
